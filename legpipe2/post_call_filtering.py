e2e26a282bf94282d71fa1dc0d9b5dfc41b824b6
ne1s0n <nelson.nazzicari@gmail.com>
2023-04-27 16:52:16 +0200

    implemented mapping quality filtering

--- legpipe2/post_call_filtering.py
+++ legpipe2/post_call_filtering.py
@@ -20,10 +20,13 @@ def interpolate(conf, raw_conf):
 	conf['post_call_filtering']['infile'] = conf['post_call_filtering']['infolder'] + '/raw_SNPs_haplo.vcf.gz'
 	conf['post_call_filtering']['outfile'] = conf['post_call_filtering']['infolder'] + '/filtered_SNPs_haplo.vcf.gz'
 	conf['post_call_filtering']['tmpfile'] = conf['post_call_filtering']['infolder'] + '/tmp.vcf.gz'
-	conf['post_call_filtering']['logfile'] = conf['post_call_filtering']['infolder'] + '/post_call_filterin.log'
+	conf['post_call_filtering']['logfile'] = conf['post_call_filtering']['infolder'] + '/post_call_filtering.log'
 	
 	#MAF
 	conf['post_call_filtering']['min_maf'] = raw_conf['post_call_filtering'].getfloat('min_maf') 
+	
+	#Mapping Quality MQ
+	conf['post_call_filtering']['min_mq'] = raw_conf['post_call_filtering'].getfloat('min_mq') 
 
 	
 	return(conf)
@@ -31,7 +34,7 @@ def interpolate(conf, raw_conf):
 def post_call_filtering(conf):
 
 	#interface
-	common.print_step_header('Running post calling filtering')
+	common.print_step_header('Post calling filtering')
 	
 	#should we do something?
 	RUN_THIS=conf['post_call_filtering']['run_this']
@@ -46,10 +49,7 @@ def post_call_filtering(conf):
 	LOGFILE=conf['post_call_filtering']['logfile']
 	REFERENCE_FILE=conf['post_call_filtering']['reference_file']
 	MIN_MAF=conf['post_call_filtering']['min_maf']
-	
-	#biallelic SNP
-	print(' - select only biallelic SNPs and filter on allele frequency (MAF): ' + str(MIN_MAF))
-	
+	MIN_MQ=conf['post_call_filtering']['min_mq']
 	
 	#FILTER ONE: biallelic
 	#https://gatk.broadinstitute.org/hc/en-us/articles/360037055952-SelectVariants
@@ -59,6 +59,7 @@ def post_call_filtering(conf):
 	#   --variant INFILE \
 	#   --output OUTFILE \
 	#	--restrictAllelesTo BIALLELIC
+	print(' - select only biallelic SNPs')
 	cmd = ['gatk', '--java-options', '-Xmx4g', 'SelectVariants']
 	cmd += ['--reference', REFERENCE_FILE]
 	cmd += ['--variant', INFILE]
@@ -68,30 +69,28 @@ def post_call_filtering(conf):
 		fp.write(' '.join(cmd) + '\n')
 		subprocess.run(cmd, shell=False, stdout=fp, stderr=subprocess.STDOUT, text=True)
 	
-	#FILTER TWO: MAF
+	#FILTER TWO: MAF, mapping quality
 	#gatk --java-options "-Xmx4g" VariantFiltration  \
 	#   --reference $REFERENCE_FILE \
 	#   --variant $OUTFILE1 \
 	#   --output $OUTFILE2 \
 	#   --filter-expression "AF <= 0.050 || AF >= 0.950" \
 	#   --filter-name "AF_0.05_0.95"
+	#   --filter-expression "MQ <= 40 \
+	#   --filter-name "MQ_40"
+	print(' - filter on minor allele frequency (MAF): ' + str(MIN_MAF))
+	print(' - filter on minimum mapping quality: ' + str(MIN_MQ))
 	cmd = ['gatk', '--java-options', '-Xmx4g', 'VariantFiltration']
 	cmd += ['--reference', REFERENCE_FILE]
 	cmd += ['--variant', TMPFILE]
 	cmd += ['--output', OUTFILE]
 	cmd += ['--filter-expression', 'AF <= ' + str(MIN_MAF) + ' || AF >= ' + str(1-MIN_MAF)]
 	cmd += ['--filter-name', 'AF_'+ str(MIN_MAF) + '_' + str(1-MIN_MAF)]
+	cmd += ['--filter-expression', 'MQ < ' + str(MIN_MQ)]
+	cmd += ['--filter-name', 'MQ_'+ str(MIN_MQ)]
 	with open(LOGFILE, "a") as fp:
 		fp.write(' '.join(cmd) + '\n')
 		subprocess.run(cmd, shell=False, stdout=fp, stderr=subprocess.STDOUT, text=True)
 
-
 	#cleanup
 	subprocess.run(['rm', TMPFILE], shell=False)
-
-
-
-
-
-
-
