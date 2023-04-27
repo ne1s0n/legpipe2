2281cf7397383eae36c7b34e1ad92bf5193a3560
ne1s0n <nelson.nazzicari@gmail.com>
2023-04-27 18:22:38 +0200

    listed the next steps

--- legpipe2/output_matrices.py
+++ legpipe2/output_matrices.py
@@ -2,7 +2,6 @@
 #fornat
 
 
-#https://www.reneshbedre.com/blog/vcf-fields.html
 
 #GT:AD:DP:GQ:PGT:PID:PL:PS       
 #0/0:4,0:4:12:.:.:0,12,117       
@@ -34,6 +33,26 @@ def output_matrices(conf):
 	if not RUN_THIS:
 		print('SKIPPED')
 		return(None)
+	
+	#reference, for future
+	#https://www.reneshbedre.com/blog/vcf-fields.html
+	#https://samtools.github.io/bcftools/bcftools.html#query
 
+	#sample list, one per row
+	#bcftools query -l filtered_SNPs_haplo.vcf.gz > sample_list.txt
+
+	#SNP info: chromosome, position, ref allele and the first alternate allele
+	#bcftools query -f '%CHROM,%POS,%REF,%ALT{0}\n' filtered_SNPs_haplo.vcf.gz > SNP_info.txt
+
+	#make a BED file: chr, pos (0-based), end pos (1-based), id
+	#bcftools query -f'%CHROM\t%POS0\t%END\t%ID\n' file.bcf > SNP_info.bed
+
+	#general structure for output
+	#bcftools view --apply-filters PASS infile.vcf.gz | bcftools query -f <some other filter> | bgzip -c > output.csv.gz
+
+	#allele count matrices: AD, DP
+	#bcftools view --apply-filters PASS filtered_SNPs_haplo.vcf.gz | bcftools query -f '[%AD]\n' | bgzip -c > output_AD.csv.gz
+	#bcftools view --apply-filters PASS filtered_SNPs_haplo.vcf.gz | bcftools query -f '[%DP]\n' | bgzip -c > output_DP.csv.gz
+	
 	print('WARNING: this is a stub function')
 	return(None)
